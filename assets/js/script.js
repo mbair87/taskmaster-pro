fb8724a32182f4e321df439959e93b489441c00b
Michael Bair <mbair87@gmail.com>
2021-11-26 19:09:07 -0700

    added drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var tasks = {};
 
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -73,6 +74,58 @@ var loadTasks = function() {
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
+//enable draggable/sortable feature on list-group elements
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    var tempArr = [];
+
+   // loop over current set of children in sortable list
+$(this).children().each(function() {
+  var text = $(this)
+    .find("p")
+    .text()
+    .trim();
+
+  var date = $(this)
+    .find("span")
+    .text()
+    .trim();
+
+    //add task data to the temp array as an object
+    tempArr.push({
+      text: text,
+      date:date
+    });
+});
+console.log(tempArr);
+
+// trim down list's ID to match object property
+var arrName = $(this)
+  .attr("id")
+  .replace("list-", "");
+
+// update array on tasks object and save
+tasks[arrName] = tempArr;
+saveTasks();
+  }
+});
+
 $(".list-group").on("click", "p", function() {
   var text = $(this)
     .text()
@@ -112,6 +165,22 @@ $(".list-group").on("blur", "input[type='text']", function() {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+});
+
+$("#trash").droppable({
+accept: ".card .list-group-item",
+tolerance: "touch",
+drop: function(event, ui) {
+  console.log("drop");
+  ui.draggable.remove();
+},
+over: function(event, ui) {
+  console.log("over");
+},
+out: function(event, ui) {
+  console.log("out");
+}
+
 });
 
 
@@ -180,7 +249,10 @@ $(".list-group").on("click", "span", function() {
   dateInput.trigger("focus");
 });
 
+
 // load tasks for the first time
 loadTasks();
+
+
 
 
